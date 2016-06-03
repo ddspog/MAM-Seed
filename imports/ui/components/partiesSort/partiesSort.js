b24a95485027e2926652fafe458b62e7610df4e3
Dênnis Dantas <ddspog@gmail.com>
2016-06-03 17:57:57 -0300

    Fix parties work button
    I've updated the styling of this button to use angular-material. But
    while doing this, I've come to the urge to fix some layout problems
    that weren't being solved on the tutorial that I was following.
    
    I've researched, and use the new directive from the last version of
    angular-material.

--- imports/ui/components/partiesSort/partiesSort.js
+++ imports/ui/components/partiesSort/partiesSort.js
@@ -6,12 +6,17 @@ import template from './partiesSort.html';
 class PartiesSort {
     constructor() {
         this.changed();
+
+        this.orderOptions = [
+            {value:  1, text: "A-Z Order"},
+            {value: -1, text: "Z-A Order"}
+        ];
     }
 
     changed() {
         this.onChange({
             sort: {
-                [this.property]: parseInt(this.order)
+              [this.property]: parseInt(this.order)
             }
         });
     }
