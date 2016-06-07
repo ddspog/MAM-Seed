df09fb78cad96c9f887f5c9c6b367bb96d5d6200
Dênnis Dantas <ddspog@gmail.com>
2016-06-07 01:45:54 -0300

    Mod travis to run with spacejam (10th try) :white_check_mark:
    Trying new command with spacejam.

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
-Package.onTest(function (api) {
-  api.use([
-    'es5-shim',
-    'pbastowski:angular-babel',
-    'practicalmeteor:chai',
-    'practicalmeteor:mocha',
-    'practicalmeteor:sinon'
-  ]);
-
-  api.addFiles('./imports/test.js');
-});
+// Package.onTest(function (api) {
+//   api.use([
+//     'es5-shim',
+//     'pbastowski:angular-babel',
+//     'practicalmeteor:chai',
+//     'practicalmeteor:mocha',
+//     'practicalmeteor:sinon'
+//   ]);
+//
+//   api.addFiles('./imports/test.js');
+// });
