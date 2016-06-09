4b0b110543c56996b5c2c910e7c7a7d1cffe52c8
Dênnis Dantas <ddspog@gmail.com>
2016-06-09 02:36:26 -0300

    Add Password tests :white_check_mark:
    I've added Password test, checking call to some method.
    
    Ref #6

---
+++ imports/ui/components/password/client/password.tests.js
@@ -0,0 +1,72 @@
+import {
+    name as Password
+} from '../password';
+
+import 'angular-mocks';
+
+import {
+    Meteor
+} from 'meteor/meteor';
+import {
+    Accounts
+} from 'meteor/accounts-base';
+
+import {
+    sinon
+} from 'meteor/practicalmeteor:sinon';
+
+describe('Password', function() {
+    spies.restoreAll();
+
+    beforeEach(function(done) {
+        window.module(Password);
+        done();
+    });
+
+    describe('controller', function() {
+        let controller;
+
+        beforeEach(function(done) {
+            inject(function($rootScope, $componentController) {
+                controller = $componentController(Password, {
+                    $scope: $rootScope.$new(true)
+                });
+            });
+            done();
+        });
+
+        it('should have credentials all empty by default', function(done) {
+            expect(controller.credentials).to.be.deep.equal({
+                email: ''
+            });
+            done();
+        });
+
+        it('should have error empty by default', function(done) {
+            expect(controller.error).to.be.equal('');
+            done();
+        });
+
+        describe('reset()', function() {
+            let validEmail = 'validEmail';
+
+            afterEach(function(done) {
+                if (spies.forgot)
+                    spies.forgot.restore();
+                done();
+            });
+
+            it('should call Accounts.forgotPassword', function(done) {
+                spies.create('forgot', Accounts, 'forgotPassword');
+
+                controller.credentials = {
+                    email: validEmail
+                };
+                controller.reset();
+
+                expect(spies.forgot).to.be.calledWith(controller.credentials);
+                done();
+            });
+        });
+    });
+});
