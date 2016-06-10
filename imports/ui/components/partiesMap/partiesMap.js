8d6a1092b2ff0f4a14fefb68125e60089897763a
Dênnis Dantas <ddspog@gmail.com>
2016-06-10 02:05:51 -0300

    Mod spacing and identation on class :art:
    I've update the spacing and indentation, using a beautify command.
    
    Ref #13

--- imports/ui/components/partiesMap/partiesMap.js
+++ imports/ui/components/partiesMap/partiesMap.js
@@ -3,7 +3,9 @@ import angularMeteor from 'angular-meteor';
 import 'angular-simple-logger';
 import 'angular-google-maps';
 
-import { ConfigGoogleMap } from '../../configs/googleMap/googleMapConfig';
+import {
+    ConfigGoogleMap
+} from '../../configs/googleMap/googleMapConfig';
 
 import template from './partiesMap.html';
 
@@ -11,30 +13,30 @@ import template from './partiesMap.html';
  * PartiesMap component
  */
 class PartiesMap {
-  constructor() {
-    this.map = {
-      center: {
-        latitude: 45,
-        longitude: -73
-      },
-      zoom: 8
-    };
-  }
+    constructor() {
+        this.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
+    }
 }
 
 const name = 'partiesMap';
 
 // create a module
 export default angular.module(name, [
-  angularMeteor,
-  'nemLogging', // https://github.com/angular-ui/angular-google-maps/issues/1633
-  'uiGmapgoogle-maps'
-]).component(name, {
-  template,
-  controllerAs: name,
-  bindings: {
-    parties: '='
-  },
-  controller: PartiesMap
-})
-  .config(ConfigGoogleMap);
+        angularMeteor,
+        'nemLogging', // https://github.com/angular-ui/angular-google-maps/issues/1633
+        'uiGmapgoogle-maps'
+    ]).component(name, {
+        template,
+        controllerAs: name,
+        bindings: {
+            parties: '='
+        },
+        controller: PartiesMap
+    })
+    .config(ConfigGoogleMap);
