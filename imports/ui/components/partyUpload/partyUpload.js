b32d05a34a0c6ad386dfebcba16b834a2e0b3bcf
Dênnis Dantas <ddspog@gmail.com>
2016-06-13 04:51:03 -0300

    Add PartyUpload component :bulb:
    I've added PartyUpload component, with the support to upload photos via
    drag and drop.
    
    Ref #20

---
+++ imports/ui/components/partyUpload/partyUpload.js
@@ -0,0 +1,49 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import ngFileUpload from 'ng-file-upload';
+import 'ng-img-crop/compile/minified/ng-img-crop';
+import 'ng-img-crop/compile/minified/ng-img-crop.css';
+
+import {
+    Meteor
+} from 'meteor/meteor';
+
+import template from './partyUpload.html';
+
+class PartyUpload {
+    constructor($scope, $reactive) {
+        'ngInject';
+
+        $reactive(this).attach($scope);
+    }
+
+    addImages(files) {
+        if (files.length) {
+            this.currentFile = files[0];
+
+            const reader = new FileReader;
+
+            reader.onLoad = this.$bindToContext((e) => {
+                this.cropImgSrc = e.target.result;
+                this.myCroppedImage = '';
+            });
+
+            reader.readAsDataUrl(files[0]);
+        } else {
+            this.cropImgSrc = undefined;
+        }
+    }
+}
+
+const name = 'partyUpload';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor,
+    ngFileUpload,
+    'ngImgCrop'
+]).component(name, {
+    template,
+    controllerAs: name,
+    controller: PartyUpload
+});
