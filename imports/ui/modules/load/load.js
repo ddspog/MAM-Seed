8c3d33229d6779d5924ddcf28005d4b2eeade109
Dênnis Dantas <ddspog@gmail.com>
2016-06-13 02:00:28 -0300

    Mod library to return caller :art:
    I've modified the library to return another function as well. A
    function to return a caller for controller components.
    
    Ref #9

--- imports/ui/modules/load/load.js
+++ imports/ui/modules/load/load.js
@@ -1,8 +1,29 @@
-export function LoadController(componentModule, deliver, done, controllerScope = {}) {
-    inject(function($rootScope, $componentController) {
-        deliver($componentController(componentModule, {
-            $scope: $rootScope.$new(true)
-        }, controllerScope));
-    });
+const RETURN_CONTROLLER = Symbol();
+const RETURN_CALLER = Symbol();
+
+function LoadSomething(component, deliver, done, controllerScope, type = RETURN_CONTROLLER) {
+    if (type === RETURN_CONTROLLER) {
+        inject(function($rootScope, $componentController) {
+            deliver($componentController(component, {
+                $scope: $rootScope.$new(true)
+            }, controllerScope));
+        });
+    } else {
+        inject(function($rootScope, $componentController) {
+            deliver(function(bindings) {
+                return $componentController(component, {
+                    $scope: $rootScope.$new(true)
+                }, bindings);
+            });
+        });
+    }
     done();
+}
+
+export function LoadController(component, deliver, done, controllerScope = {}) {
+    LoadSomething(component, deliver, done, controllerScope, RETURN_CONTROLLER);
+}
+
+export function LoadCaller(component, deliver, done, controllerScope = {}) {
+    LoadSomething(component, deliver, done, controllerScope, RETURN_CALLER);
 }
